70d2ad8ad6f360fe26290a8c434e2d0fd794143f
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-21 00:14:13 -0600

    Wiring up google oauth

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 const app = express()
 const port = 4000
 
 // register google oauth with passport
-passport.use(new GoogleStrategy())
+passport.use(new GoogleStrategy({
+  clientID: keys.GOOGLE_CLIENT_ID,
+  clientSecret: = keys.GOOGLE_CLIENT_SECRET
+}))
 
 app.get('/', (req, res) => res.send({hi:'there'}))
 
