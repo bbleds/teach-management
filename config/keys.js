65fb44fe31805a063168e27b250da0a774fe0210
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-21 00:05:42 -0600

    Setting up basic structure and beginning oauth process

---
+++ config/keys.js
@@ -0,0 +1,1 @@
+// include and export keys as needed here
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
 const port = 4000
+
+// register google oauth with passport
+passport.use(new GoogleStrategy())
 
 app.get('/', (req, res) => res.send({hi:'there'}))
 
