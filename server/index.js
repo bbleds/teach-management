7e12a8584d741abed7562d6bcae866e7edb469d6
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-23 12:14:42 -0600

    Integrating mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
 const app = express()
 const PORT = process.env.port || 4000
+
+mongoose.connect( keys.MONGODB_URI )
 
 require('./services')
 require('./routes')(app)
