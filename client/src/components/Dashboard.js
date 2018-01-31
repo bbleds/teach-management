38bc86d67c2d0537540a6e9748d7e83524c23f37
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-31 04:55:05 -0600

    Adding redirect handler for protected routes on client side

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Redirect } from 'react-router-dom'
+import actions from '../actions'
 
-export default () => {
-  return(
-    <h1>Dashboard</h1>
-  )
+class Dashboard extends Component{
+	render(){
+		const content = this.props.user === false ?
+			(<Redirect to="/" />) :
+			(<h1>Dashboard</h1>)
+
+		return(
+			<div>
+				{content}
+			</div>
+		)
+	}
 }
+
+const mapStateToProps = state => {
+  return {
+    user: state.auth.user,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(actions, dispatch),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
